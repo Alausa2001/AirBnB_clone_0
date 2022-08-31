09ebac8d2c13495578a6749e2d18771d292e1b58
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-08-31 01:13:39 +0100

    This is the base class for the project

---
+++ models/base_model.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module contain the parent class from which other classes inherit"""
+
+
+class BaseModel():
+    """This class defines all common attributes/methods for other classes"""
