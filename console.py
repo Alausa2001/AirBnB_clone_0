9b8f27fa8cfea1f0c423989898ad23b5f70305f1
Alausa Aishat <qtaisha.ao@gmail.com>
2022-09-02 07:45:11 +0100

    initial build of console with some command functionalities

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module contains the entry point of the command interpreter"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """This class handles the implementation of the
+    command interpreter attributes"""
+
+    # def do_help(self, arg: str) -> 'type' | 'NoneType':
+    #     """lists commands with detailed help"""
+    #     return super().do_help(arg)
+
+    def do_EOF(self, line):
+        """exits the program"""
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        raise SystemExit
+
+    def do_emptyline(self):
+        """an emptyline + ENTER executes nothing"""
+        pass
+
+    prompt = "(hbnb) "
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
