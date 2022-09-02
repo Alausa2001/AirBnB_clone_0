336142fa3dfec01b84b3ea5702365533567b1d0e
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-02 12:25:54 +0100

    corrected the import statements

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@
 
 from datetime import datetime
 import uuid
-#storage = __import__('models.__init__').storage
 import models
 
 time_format = "%Y-%m-%dT%H:%M:%S.%f"
@@ -47,7 +46,7 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the
         instance"""
-        dict_copy = self.__dict__
+        dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
         dict_copy['created_at'] = dict_copy['created_at'].strftime(time_format)
         dict_copy['updated_at'] = dict_copy['updated_at'].strftime(time_format)
