525a859e0ce0740ff7f9879c8d3e3c3bacf5d9fe
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-05 00:12:57 +0100

    updated the console to work for other classes

--- console.py
+++ console.py
@@ -5,8 +5,14 @@
 import cmd
 from models.base_model import BaseModel
 from models.__init__ import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-classes = ['BaseModel']
+classes = ['BaseModel', 'User', 'Place', 'State', 'City', 'Amenity', 'Review']
 
 
 class HBNBCommand(cmd.Cmd):
