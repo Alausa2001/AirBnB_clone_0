b0179f2cb6eb7339fdf2bb94a9244f6ccf2aaa5b
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-25 13:16:25 +0100

    updated the console to carry out update command in a more advanved way

--- console.py
+++ console.py
@@ -20,24 +20,31 @@ class HBNBCommand(cmd.Cmd):
     command interpreter attributes"""
 
     def precmd(self, line):
-        new = line.split(".", 1)
+        new = line.split(".")
         if len(new) == 2:
-            if new[0] == 'update':
-                pass
+            if 'update' in new[1]:
+                classname, cmddetails = new
+                cmddetails = cmddetails.split('(')
+                cmd_name, details = cmddetails
+                details = details.rstrip(')')
+                details = details.split(' ')
+                details_list = list()
+                for detail in details:
+                    detail = detail.rstrip(',')
+                    detail = detail.replace('"', '')
+                    details_list.append(detail)
+                id_, key, value = details_list
+                line = cmd_name + ' ' + classname + ' '
+                line += id_ + ' ' + key + ' ' + value
+                return (line)
+
             else:
                 cls_nm, cmd_all = new
                 command, cmd_info = cmd_all.split("(")
                 cmd_info = cmd_info.rstrip(")")
                 cmd_info = cmd_info.replace('"', '')
                 line = command + " " + cls_nm + " " + cmd_info
-                return line
-        """
-        elif len(new) > 2:
-            cls_nm, cmd_all = new
-            command, cmd_info = cmd_all.split("(")
-            cmd_info = cmd_info.rstrip(")")
-            cmd_info = cmd_info.replace('"', '')
-            line = """
+                return (line)
         return cmd.Cmd.precmd(self, line)
 
     def do_EOF(self, line):
@@ -53,7 +60,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """creates an instance of BaseModel"""
+        """creates an instance of a class. Example: $ create BaseModel"""
         if len(arg) == 0:
             print("** class name missing **")
         elif arg in classes:
@@ -65,7 +72,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """prints the string representaion of an instance
-        based on the class name and id"""
+        based on the class name and id.\
+                Example: show BaseModel 1234-1234-1234"""
         if len(arg) == 0:
             print("** class name missing **")
             return False
@@ -85,7 +93,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id
-        (save the change into the JSON file)."""
+        (save the change into the JSON file).
+        Example:  destroy BaseModel 1234-1234-1234"""
         if len(arg) == 0:
             print("** class name missing **")
             return False
@@ -106,7 +115,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         """prints all string representation of all instances
-        based or not on the class name."""
+        based or not on the class name.
+        Example: all BaseModel or  all"""
         cmd_arg = arg.split()
         all_list = []
         if len(arg) > 0:
@@ -115,7 +125,7 @@ class HBNBCommand(cmd.Cmd):
                 for key in all_dict.keys():
                     ref = key.split('.')
                     if ref[0] == cmd_arg[0]:
-                        all_list.append(str(all_dict[key]))
+                        all_list.append(all_dict[key])
                 print(all_list)
             else:
                 print("** class doesn't exist **")
@@ -126,7 +136,8 @@ class HBNBCommand(cmd.Cmd):
             print(all_list)
 
     def do_update(self, arg):
-        """update an instance based on class and id"""
+        """update an instance based on class and id
+        Usage: update <class name> <id> <attribute name> <attribute value>"""
         c_arg = arg.split()
         show = storage.all()
         if len(c_arg) == 0:
@@ -137,11 +148,10 @@ class HBNBCommand(cmd.Cmd):
                 class_id = '.'.join([c_arg[0], c_arg[1]])
                 if class_id in show:
                     if len(c_arg) > 2:
-                        if len(c_arg) > 3:
-                            for key in show:
-                                c_arg[3] = eval(c_arg[3])
-                                show[key].__dict__[c_arg[2]] = c_arg[3]
-                                storage.save()
+                        if len(c_arg) == 4:
+                            c_arg[3] = eval(c_arg[3])
+                            show[class_id].__dict__[c_arg[2]] = c_arg[3]
+                            storage.save()
                         else:
                             print("** value missing **")
                     else:
@@ -154,7 +164,8 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def do_count(self, arg):
-        """ retrieve the number of instances of a class"""
+        """ retrieve the number of instances of a class
+        Usage: count classname"""
         count = 0
         cmd_arg = [arg]
         for key in storage.all().keys():
